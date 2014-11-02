b0d5631ccf4c4725603173c2d27a4d9f32bedfd9
Richmond Watkins <richmondwatkins@mac.com>
2014-11-02 04:31:19 -0600

    updates

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,11 @@
 class Device < ActiveRecord::Base
 
   def tester
-    device = Device.find_or_create_by(:deviceid => "WORKED WORKED")
+    device = Device.find_or_create_by(:deviceid => params[:device])
+    put '=================='
+    puts device.deviceid  
+    put '=================='
+
   end
 
 end
