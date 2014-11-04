9a4c9b8da43d400a1410b89aa2676645f22c2eb1
Richmond Watkins <richmondwatkins@mac.com>
2014-11-03 22:05:09 -0600

    updates

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -51,8 +51,11 @@ class DevicesController < ApplicationController
     puts '==============================='
     puts '==============================='
     puts '==============================='
-
-  end
+    respond_to do |format|
+      msg = { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end 
+   end
 
   private
 
