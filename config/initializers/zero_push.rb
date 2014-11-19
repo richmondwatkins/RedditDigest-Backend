50dca8063e521ef2dece2501950f9769f91891e0
Richmond Watkins <richmondwatkins@mac.com>
2014-11-18 19:15:45 -0600

    changed production push key

--- config/initializers/zero_push.rb
+++ config/initializers/zero_push.rb
@@ -1,5 +1,5 @@
-if Rails.env.production?
-  ZeroPush.auth_token = 'z3C5aT8qdyePAnD1eq9n'
-else
-  ZeroPush.auth_token = 'eZHW1pzRaYJHua6Egswr'
-end
+# if Rails.env.production?
+#   ZeroPush.auth_token = 'z3C5aT8qdyePAnD1eq9n'
+# else
+#   ZeroPush.auth_token = 'eZHW1pzRaYJHua6Egswr'
+# end
